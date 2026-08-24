4271506a9cf111246171efb18eb8c2ace7ad5622
RAJAVIGOR <rajavigorteam@gmail.com>
2026-08-24 17:31:46 +0700

    Create menu.js

---
+++ config/menu.js
@@ -0,0 +1,37 @@
+window.BLOGGER_MASTER_MENU = {
+
+  items: [
+
+    {
+      title: "HOME",
+      url: "/"
+    },
+
+    {
+      title: "DEPOSIT",
+      url: "/p/deposit.html"
+    },
+
+    {
+      title: "WITHDRAW",
+      url: "/p/withdraw.html"
+    },
+
+    {
+      title: "INFORMASI",
+      url: "/p/informasi.html"
+    },
+
+    {
+      title: "ABOUT US",
+      url: "/p/about-us.html"
+    },
+
+    {
+      title: "DAFTAR BLOG INDEX",
+      url: "/p/blog-index.html"
+    }
+
+  ]
+
+};

6812527172961d5f9695fabc2760f1f1ea7e62d3
RAJAVIGOR <rajavigorteam@gmail.com>
2026-08-24 17:46:32 +0700

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -1,66 +1,25 @@
 window.BLOGGER_MASTER_MENU = {
 
-  items: [
+  html: `
+    <div class="main-menu section" id="main-menu" name="Main Menu"><div class="widget LinkList show-menu" data-version="2" id="LinkList74">
+<ul id="main-menu-nav" role="menubar">
+<li class="li-home li-home-icon"><a href="/" role="menuitem" data-title="Home"><i class="fa fa-home"></i></a></li>
+<li class="has-sub"><a href="#" role="menuitem">SITUS SLOT 10K</a><ul class="sub-menu m-sub"><li><a href="https://jaga.link/rajavigor" role="menuitem">RAJAVIGOR</a></li><li><a href="https://jaga.link/kantinslot" role="menuitem">KANTINSLOT</a></li><li><a href="https://rumahsiapa.com/" role="menuitem">SLOTKAMBOJA</a></li><li><a href="https://jaga.link/romawibet" role="menuitem">ROMAWIBET</a></li></ul></li>
 
-    {
-      title: "⌂",
-      url: "/",
-      className: "home"
-    },
 
-    {
-      title: "SITUS SLOT 10K",
-      url: "#",
-      children: [
-        {
-          title: "RAJAVIGOR",
-          url: "https://jaga.link/rajavigor"
-        },
-        {
-          title: "KANTSLOT",
-          url: "https://jaga.link/kantslot"
-        },
-        {
-          title: "SLOTKAMBO",
-          url: "https://rumaslapa.com/"
-        },
-        {
-          title: "ROMAWIBET",
-          url: "https://jaga.link/romawibet"
-        }
-      ]
-    },
 
-    {
-      title: "DEPOSIT",
-      url: "#"
-    },
 
-    {
-      title: "WITHDRAW",
-      url: "#"
-    },
+<li><a href="#" role="menuitem">DEPOSIT</a></li>
+<li><a href="#" role="menuitem">WITHDRAW</a></li>
+<li><a href="#" role="menuitem">INFORMASI</a></li>
+<li><a href="#" role="menuitem">ABOUT US</a></li>
+<li class="has-sub"><a href="#" role="menuitem">LIVECHAT</a><ul class="sub-menu m-sub"><li><a href="#" role="menuitem">CS RAJAVIGOR</a></li><li><a href="#" role="menuitem">CS KANTINSLOT</a></li><li><a href="#" role="menuitem">CS ROMAWIBET</a></li></ul></li>
 
-    {
-      title: "INFORMASI",
-      url: "#"
-    },
 
-    {
-      title: "ABOUT US",
-      url: "#"
-    },
 
-    {
-      title: "LIVECHAT",
-      url: "#"
-    },
-
-    {
-      title: "DAFTAR BLOG INDEX",
-      url: "#"
-    }
-
-  ]
+<li><a href="#" role="menuitem">DAFTAR BLOG INDEX</a></li>
+</ul>
+</div></div>
+  `
 
 };

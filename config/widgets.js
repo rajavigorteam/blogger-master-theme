f05929a9c8b61dc622a0475516acc302378f73e1
RAJAVIGOR <rajavigorteam@gmail.com>
2026-08-24 17:49:42 +0700

    Update widgets.js

--- config/widgets.js
+++ config/widgets.js
@@ -47,6 +47,7 @@ window.BLOGGER_MASTER_WIDGETS = {
 </ul>
 
       </div>
+      </br>
       <div class="_1dro _2ph- clearfix"><a class="_3-8_ lfloat" href="https://www.facebook.com/1250491084816518?ref=embed_page" target="_blank"><img class="_1drn _-s img" src="https://scontent-bkk1-2.xx.fbcdn.net/v/t39.30808-1/781005547_122109619035433194_8399284091987175678_n.jpg?stp=cp0_dst-jpg_s50x50_tt6&amp;_nc_cat=109&amp;ccb=1-7&amp;_nc_sid=f907e8&amp;_nc_ohc=gzp7_JZINL4Q7kNvwH1M1J0&amp;_nc_oc=Ado0rYaHQhrGxJGXp2ghOgtwJdUYTV2Q2YeECenNRrfJtdVpGeVwxbdWAu-yEipDl_E&amp;_nc_zt=24&amp;_nc_ht=scontent-bkk1-2.xx&amp;edm=AK-Ac0AEAAAA&amp;_nc_gid=gGMZhspBPwsOL3BQZMjRZQ&amp;oh=00_AQF4kHzMlZ7dETe73mWkLw8Xo_nqq5DXHLBbYKyU7ka7-g&amp;oe=6A91DDD4" height="50" width="50" alt=""></a><div class="lfloat"><div><a style="max-width: 200px;" class="_1drp _5lv6" title="Rajavigorgames" href="https://www.facebook.com/1250491084816518?ref=embed_page" target="_blank">Rajavigorgames</a></div><div class="_1drq" style="max-width: 220px;"></div>
     `
 

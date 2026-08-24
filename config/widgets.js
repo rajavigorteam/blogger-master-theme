a6b90f20846d84ded5efabbe33840a0435b74312
RAJAVIGOR <rajavigorteam@gmail.com>
2026-08-24 17:44:10 +0700

    Update widgets.js

--- config/widgets.js
+++ config/widgets.js
@@ -19,11 +19,6 @@ window.BLOGGER_MASTER_WIDGETS = {
         </p>
 
       </div>
-      <div class="widget-content">
-<p><a title="MASTERMPO" href="https://qoolink.co/rajavigorgames" target="blank"><img loading="lazy" src="https://i.postimg.cc/gJ6329tP/slot-qris.png" alt="online" width="100%" height="auto"></a></p>
-<iframe width="100%" height="auto" src="https://www.youtube.com/embed/Zft3UjQ2dYA" title="Geolife Vigore Raja  The Key to a Healthier Harvest and Happier Farms" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
-<iframe src="https://secure.livechatinc.com/customer/action/open_chat?license_id=16664613" width="100%" height="500px" frameborder="0"></iframe>
-</div>
     `
 
   },
